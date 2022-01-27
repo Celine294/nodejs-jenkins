dba518b654fbff827b4ebd0e316fc42cc54d122a
Céline YE <celine.ye96@gmail.com>
2022-01-27 16:14:00 +0100

    Changed a random text to check if Jenkins works

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, Docker and Jenkins!
         </a>
       </header>
     </div>
